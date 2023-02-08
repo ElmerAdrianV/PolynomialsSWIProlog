4c91761f86a05490df0288a669fa0647235d722a
elmeradrianv <elmeradrianv@outlook.com>
2023-02-08 13:33:26 -0600

    imprime poly borrador

---
+++ ImprimePolinomio.pl
@@ -0,0 +1,75 @@
+/*
+clasifica(X):-
+	write(X),nl,
+	(	X<0 ->
+		write("El argumento es negativo."),nl
+	;	
+	    write("El argumento es positivo."),nl	
+	).
+*/
+write_polynomial(_,_, []):-
+    nl,!.
+
+write_polynomial(Degree, 0, [Coef,Poly]):-
+    write("entre"),nl,
+    write(Coef),
+    write_polynomial(Degree, 1, Poly).
+
+write_polynomial(Degree, I, [Coef,Poly]):-
+    (	Coef<0 ->
+		   write(Coef)
+	;   write("+"),write(Coef)
+	),
+    write("X^"),write(I),
+    write_polynomial(Degree, I, [Coef,Poly]).
+
+write_polynomial(Poly):-
+    calcula_grado(Poly, Degree),
+    write_polynomial(Degree, 0,Poly).
+
+
+
+/*
+Regla que calcula el grado de un polinomio pelón
+*/
+
+/*Recibe un polinomio "pelón" solo con coeficientes
+Regresa el mismo polinomio pero con el grado pegado
+
+agrega_grado(i,o).
+*/
+
+calcula_grado(Polinomio,Grado):-
+    find_last_non_zero(Polinomio,Grado).
+
+
+
+/* Caso base: determinamos que la posición es el último cero registrado
+*/
+find_last_non_zero([],_,LastNonZero,Position) :-
+Position is LastNonZero,
+!.
+
+
+/*
+Vamos vaciando la lista, cada que encontramos un cero
+actualizamos la posición del último cero
+*/
+find_last_non_zero([X|Y],Count,LastNonZero,Position):-
+
+(X =\= 0 ->  % Si encontramos un no cero
+    NewCount is Count+1, %Seguimos la cuenta
+    NewLNZ is Count, %Guardamos su posición como la del ultimo cero encontrado
+    find_last_non_zero(Y,NewCount,NewLNZ,Position)
+    ;
+    %Si no hay cero pasamos el LastNonZero anterior
+    NewCount is Count+1,
+    find_last_non_zero(Y,NewCount,LastNonZero,Position)).
+
+/*
+Regla para encontrar el último no cero en una lista y regresar su posición
+
+find_last_non_zero(i,o).
+*/
+find_last_non_zero(Lista,Position):-
+    find_last_non_zero(Lista,0,0,Position).
